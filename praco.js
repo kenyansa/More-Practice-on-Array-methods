033962b6a5e22d4e5a10847b2fcdcd029425b27a
kenyansa <kenyansajustine@gmail.com>
2022-04-28 20:31:19 +0300

    return the first number the method find() finds

--- praco.js
+++ praco.js
@@ -16,3 +16,8 @@ persons.map(getFullName);
 function getFullName(item) {
   return [item.firstname, item.lastname].join(" ");
 }
+//Array.prototype.find(): returns the first element in the provided array that satisfies the provided testing function.
+const array = [2,3,4,5,6,7, 40,45];
+const bigNum = array.find(element=>element>30);
+console.log(bigNum);
+
