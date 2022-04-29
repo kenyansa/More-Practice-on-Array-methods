0da90e4e0df77843c87e394575cff2240d0d77c6
kenyansa <kenyansajustine@gmail.com>
2022-04-29 11:23:34 +0300

    use map method to append a message to every array element with a function

--- map-method.js
+++ map-method.js
@@ -5,4 +5,14 @@ const rollCall = [];
 for (const student of students) {
   rollCall.push(student + " the wizard");
 }
+//calling the empty array
 console.log(rollCall)
+
+// using map() with a function declaration
+function studentRollCall(studentName){
+    return studentName + " the wizard"
+}
+const studentNames = ["Justine", "Hera", "Matilda", "Ruth"];
+const rollCallz = studentNames.map(studentRollCall);
+//calling the created array
+console.log(rollCallz);
