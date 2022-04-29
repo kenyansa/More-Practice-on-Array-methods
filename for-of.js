7fda1a906f537bcd11d534d6aaae6780746a4fcb
kenyansa <kenyansajustine@gmail.com>
2022-04-29 09:03:49 +0300

    for--of use to tierate through objects

---
+++ for-of.js
@@ -0,0 +1,23 @@
+//used to iterate through objects
+// syntax: for (variable of iterable) {
+//   statement;
+// }
+//example 1
+const iterable = [10, 20, 30];
+
+for (const value of iterable) {
+  console.log(value);
+}
+// example 2
+const iterabo = [10, 20, 30];
+
+for (let value of iterabo) {
+  value += 1;
+  console.log(value);
+}
+//example 3: iterating over a set
+const xx = new Set([1, 1, 2, 2, 3, 3]);
+
+for (const value of xx) {
+  console.log(value);
+}
